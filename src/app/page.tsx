cdf2fc5ecdad51fb3c475fde691c627b7d4388fa
Riina Maria Korpela <riina.mk@coditramuntana.com>
2024-03-17 20:21:00 +0100

    feat: embed twitter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import { Suspense } from "react";
+import { unstable_cache } from "next/cache";
+import { TweetSkeleton, EmbeddedTweet, TweetNotFound } from "react-tweet";
+import { getTweet as _getTweet } from "react-tweet/api";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "../lib/api";
 
+const getTweet = unstable_cache(
+  async (id: string) => _getTweet(id),
+  ["tweet"],
+  { revalidate: 3600 * 24 },
+);
+
+const TweetPage = async ({ id }: { id: string }) => {
+  try {
+    const tweet = await getTweet(id);
+    return tweet ? <EmbeddedTweet tweet={tweet} /> : <TweetNotFound />;
+  } catch (error) {
+    console.error(error);
+    return <TweetNotFound error={error} />;
+  }
+};
 export default function Index() {
   const allPosts = getAllPosts();
 
@@ -16,7 +35,11 @@ export default function Index() {
       </section>
       {allPosts.length > 0 && <MoreStories posts={allPosts} />}
       <section className="flex mt-5 mb-5">
-        <div className="w-4/5">añadir twitter o instagram</div>
+        <div className="w-4/5">
+          <Suspense fallback={<TweetSkeleton />}>
+            <TweetPage id={"1768612496481366120"} />
+          </Suspense>
+        </div>
       </section>
     </>
   );

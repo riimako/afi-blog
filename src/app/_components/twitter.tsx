65408f2de5d06be9f01b63e0d673ace521e3b056
Riina Maria Korpela <riina.mk@coditramuntana.com>
2024-03-18 21:14:55 +0100

    fix: twitter doesnt allow cors

--- src/app/_components/twitter.tsx
+++ src/app/_components/twitter.tsx
@@ -1,5 +1,4 @@
-"use client";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense } from "react";
 import { getTweet as _getTweet } from "react-tweet/api";
 import { unstable_cache } from "next/cache";
 import { TweetSkeleton, EmbeddedTweet, TweetNotFound } from "react-tweet";
@@ -20,32 +19,6 @@ const TweetPage = async ({ id }: { id: string }) => {
   }
 };
 const Twitter = () => {
-  const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch(
-      "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=gerbertstc&count=1",
-      {
-        headers: {
-          oauth_consumer_key: "yRO286UmThFDy5rRTJQxQekNg",
-          oauth_consumer_secret:
-            "DHZBry7MBw4Lj7nTMgRBxaPzgioOonOB2WcUkI73BdMmPXk3Ij",
-          oauth_token: "Vzg2bGRRSFpadTd0LXBwWFhhNFg6MTpjaQ",
-          oauth_token_secret:
-            "oS6GKKlCsweyVN6YA9RXHvGOsy6pd1L8DVb_JJyNT5m6ZJ77xl",
-        },
-      },
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
-  }, []);
-
-  if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No profile data</p>;
   return (
     <section className="flex mt-5 mb-5">
       <div className="w-4/5">
